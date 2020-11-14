ab248b75e30a350e9c1b2edfa19264b01daa82af
Vlad <vladi.nikolov@gmail.com>
2020-11-13 19:36:58 -0800

    Cosmetic changes and comments

--- safecracker50.py
+++ safecracker50.py
@@ -2,12 +2,14 @@ import numpy as np
 from collections import deque
 
 
+# Rotate a ring
 def rotate(r):
     r = deque(r)
-    r.rotate(-1)
+    r.rotate(1)
     return list(r)
 
 
+# Build the ring in play
 def build(r, s):
     n = [0] * 16
     for i in range(0, 16):
@@ -15,13 +17,17 @@ def build(r, s):
     return n
 
 
-def calc(r1, r2, r3, r4, r5):
-    state = np.array([r1, r2, r3, r4, r5])
-    tot = np.sum(state, axis=0)
-    return (len(set(tot)) == 1) and tot[0] == 50
+# Calculate all positions and check if the sum matches the expected value
+def check(r0, r1, r2, r3, r4):
+    exp: int = 50
+    s = np.array([r0, r1, r2, r3, r4])
+    t = np.sum(s, axis=0)
+    return (len(set(t)) == 1) and t[0] == exp
 
 
+# Run the solution
 def main():
+    # Rings, top down. The shadow is the one under the ring.
     ring0 = [8, -1, 8, -1, 16, -1, 19, -1, 8, -1, 17, -1, 6, -1, 6, -1]
     shadow0 = [6, 18, 8, 17, 4, 20, 4, 14, 4, 5, 1, 14, 10, 17, 10, 5]
     ring1 = [10, -1, 14, -1, 11, -1, 8, -1, 12, -1, 11, -1, 3, -1, 8, -1]
@@ -31,17 +37,23 @@ def main():
     ring3 = [-1, 10, -1, 8, -1, 10, -1, 9, -1, 8, -1, 8, -1, 9, -1, 6]
     shadow3 = [10, 10, 10, 15, 7, 19, 18, 2, 9, 27, 13, 11, 13, 10, 18, 10]
     ring4 = [10, 1, 10, 4, 5, 3, 15, 16, 4, 7, 0, 16, 8, 4, 15, 7]
-    for l3 in range(0, 16):
-        for l2 in range(0, 16):
-            for l1 in range(0, 16):
-                for l0 in range(0, 16):
+
+    count = 0
+    for i3 in range(0, 16):
+        for i2 in range(0, 16):
+            for i1 in range(0, 16):
+                for i0 in range(0, 16):
+                    count += 1
+                    # Establish the ring numbers at the current position
                     level1 = build(ring0, shadow0)
                     level2 = build(ring1, shadow1)
                     level3 = build(ring2, shadow2)
                     level4 = build(ring3, shadow3)
                     level5 = ring4
-                    if calc(level1, level2, level3, level4, level5):
-                        print(f'Solved!')
+                    
+                    # Check if the current position solves the problem
+                    if check(level1, level2, level3, level4, level5):
+                        print(f'Solved! Took only {count} operations...')
                         print(f'{60 * "-"}')
                         print(f'Level 1: {level1}')
                         print(f'Level 2: {level2}')
@@ -49,13 +61,21 @@ def main():
                         print(f'Level 4: {level4}')
                         print(f'Level 5: {level5}')
                         print(f'{60 * "-"}\n')
-                        print(f'Positions - Ring1: {l0}, Ring2: {l1}, Ring3: {l2}, Ring4: {l3}')
+                        print(f'Positions - Ring 1 ({i0}), Ring 2 ({i1}), Ring 3 ({i2}), Ring 4 ({i3})')
                         return
+                    
+                    # Rotate the top ring
                     ring0 = rotate(ring0)
+                    
+                # Rotate the second ring
                 ring1 = rotate(ring1)
                 shadow0 = rotate(shadow0)
+                
+            # Rotate the third ring
             ring2 = rotate(ring2)
             shadow1 = rotate(shadow1)
+            
+        # Rotate the forth ring
         ring3 = rotate(ring3)
         shadow2 = rotate(shadow2)
 
